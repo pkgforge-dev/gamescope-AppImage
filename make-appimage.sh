776c24934a0329ae4062df45f63bad5bae3e70af
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-05-01 12:56:34 -0400

    get app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,22 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q gamescope | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=DUMMY
+export DESKTOP=DUMMY
+export MAIN_BIN=gamescope
+export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/gamescope*      \
+	/usr/lib/*gamescope*.so* \
+	/usr/share/gamescope     \
+	/usr/share/vulkan/implicit_layer.d
 
 # Additional changes can be done in between here
 
